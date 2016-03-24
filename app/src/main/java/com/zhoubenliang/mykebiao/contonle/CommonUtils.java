e3e78f70c80101133fbcb1aa615e4cbc8d4c9a65
zhoudada <554524787@qq.com>
2016-03-24 19:50:36 +0800

    2016.3.24.

---
+++ app/src/main/java/com/zhoubenliang/mykebiao/contonle/CommonUtils.java
@@ -0,0 +1,8 @@
+package com.zhoubenliang.mykebiao.contonle;
+
+public class CommonUtils {
+    public final static String BMOB_APPID = "5a17b0df45a2368bc7ad41b64cda5a27";
+    public final static String FACE_API_KEY="bbab5072f27a0d85c080b18249c68dfc";
+    public final static String FACE_API_SECRET="yK_4d7RJrcfdVoNUjK9bCNGmHXhoAJdH";
+
+}
--- app/src/main/java/com/zhoubenliang/mykebiao/view/RegisterActivity.java
+++ app/src/main/java/com/zhoubenliang/mykebiao/view/RegisterActivity.java
@@ -1,31 +1,27 @@
 package com.zhoubenliang.mykebiao.view;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.zhoubenliang.mykebiao.R;
+import com.zhoubenliang.mykebiao.contonle.CommonUtils;
+
+import cn.bmob.v3.Bmob;
 
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bmob.initialize(this, CommonUtils.BMOB_APPID);
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.navigation_back_selector);
+        toolbar.setTitle("用户注册");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
