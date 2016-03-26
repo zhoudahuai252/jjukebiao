fc2f67e4b26aa59d8b3f8c1d91d64711e646d9e5
zhoudahuai252 <554524787@qq.com>
2016-03-26 16:24:56 +0800

    弄啥呢?瞅啥呢??

--- app/src/main/java/com/zhoubenliang/mykebiao/contonle/CommonUtils.java
+++ app/src/main/java/com/zhoubenliang/mykebiao/contonle/CommonUtils.java
@@ -1,8 +1,40 @@
 package com.zhoubenliang.mykebiao.contonle;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+
+import java.io.ByteArrayOutputStream;
+
 public class CommonUtils {
     public final static String BMOB_APPID = "5a17b0df45a2368bc7ad41b64cda5a27";
     public final static String FACE_API_KEY="bbab5072f27a0d85c080b18249c68dfc";
     public final static String FACE_API_SECRET="yK_4d7RJrcfdVoNUjK9bCNGmHXhoAJdH";
+    public final  static  String FACE_SET_NAME="";
+    public static ProgressDialog getProcessDialog(Context context, String tips){
+        ProgressDialog dialog = new ProgressDialog(context);
+        dialog.setMessage(tips);
+        dialog.setCancelable(false);
+        return dialog;
+    }
+    /**
+     * 图片处理类
+     */
+    public static byte[] ImageProcess(String path) {
+        Bitmap img = BitmapFactory.decodeFile(path);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        float scale = Math.min(1,
+                Math.min(600f / img.getWidth(), 600f / img.getHeight()));
+        Matrix matrix = new Matrix();
+        matrix.postScale(scale, scale);
 
+        Bitmap imgSmall = Bitmap.createBitmap(img, 0, 0, img.getWidth(),
+                img.getHeight(), matrix, false);
+
+        imgSmall.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] array = stream.toByteArray();
+        return array;
+    }
 }
--- app/src/main/java/com/zhoubenliang/mykebiao/view/RegisterActivity.java
+++ app/src/main/java/com/zhoubenliang/mykebiao/view/RegisterActivity.java
@@ -1,20 +1,39 @@
 package com.zhoubenliang.mykebiao.view;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facepp.error.FaceppParseException;
+import com.facepp.http.HttpRequests;
+import com.facepp.http.PostParameters;
 import com.jakewharton.rxbinding.support.v7.widget.RxToolbar;
 import com.jakewharton.rxbinding.view.RxView;
 import com.zhoubenliang.mykebiao.R;
 import com.zhoubenliang.mykebiao.contonle.CommonUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,12 +41,20 @@ import java.util.regex.Pattern;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.listener.FindListener;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final Integer FACE_NO = 2;
+    private static final Integer FACE_ERROR = 3;
     @Bind(R.id.toolbar)
     Toolbar toolbar;
     @Bind(R.id.username)
@@ -46,6 +73,13 @@ public class RegisterActivity extends AppCompatActivity {
     Button btnImg;
     @Bind(R.id.btn_register)
     Button btnRegister;
+    private String ImageName = "";// 图片名称
+    private String ImageUrl = "";// 图片地址
+    FileOutputStream OutPut = null;// 输出流
+    private String TAG = "RegisterActivity";
+    private String face_id;
+    private final static Integer FACE_EXIST = 1;
+    private boolean isFaceAvaiable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,12 +93,29 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void initBtn() {
-        RxView.clicks(btnImg).subscribe(new Action1<Void>() {
+        btnImg.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void call(Void aVoid) {
-
+            public void onClick(View v) {
+                Log.d("RegisterActivity", "0000000");
+                 Intent it = new Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                it.setType("image/*");
+                startActivityForResult(it, 2);
             }
         });
+        /*RxView.clicks(btnImg).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+                Log.d("RegisterActivity", "0000000");
+
+               *//* Intent it = new Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                it.setType("image*//**//*");
+                startActivityForResult(it, 2);*//*
+            }
+        });*/
         RxView.clicks(btnRegister).subscribe(new Action1<Void>() {
             @Override
             public void call(Void aVoid) {
@@ -90,7 +141,6 @@ public class RegisterActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Log.d("RegisterActivity", "text");
                                     Toast.makeText(RegisterActivity.this, "用户名为空", Toast.LENGTH_SHORT).show();
                                 }
                             });
@@ -102,8 +152,41 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-    private void checkUser(String userString) {
+    /**
+     * \
+     * 检测用户名是否可用
+     *
+     * @param userString
+     */
+    private void checkUser(final String userString) {
+        BmobQuery<BmobUser> query = new BmobQuery<BmobUser>();
+        query.addWhereEqualTo("username", userString);
+        query.findObjects(this, new FindListener<BmobUser>() {
+            @Override
+            public void onSuccess(final List<BmobUser> object) {
 
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (object.size() > 0) {
+                            Toast.makeText(RegisterActivity.this, "用户名已经存在", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "用户名可用", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onError(int code, String msg) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(RegisterActivity.this, "网络错误!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 
     private void btnPhotoClick() {
@@ -113,8 +196,170 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(RegisterActivity.this, "SD卡不存在",
                     Toast.LENGTH_LONG).show();
             return;
+        } else if (checkpwd()) {
+            Toast.makeText(RegisterActivity.this, "两次密码不一致请重新输入",
+                    Toast.LENGTH_LONG).show();
+            return;
         } else {
-            Toast.makeText(RegisterActivity.this, "测试", Toast.LENGTH_SHORT).show();
+            ImageName = new DateFormat().format("yyyyMMdd_hhmmss",
+                    Calendar.getInstance(Locale.CHINA)) + ".jpg";
+            Log.i(TAG, "图片名称" + ImageName);
+            ImageUrl = Environment.getExternalStorageDirectory()
+                    .toString()
+                    + File.separator
+                    + "JJukebiao"
+                    + File.separator + ImageName;
+            Log.i(TAG, "图片地址ImageURL为" + ImageUrl);
+            File file = new File(Environment
+                    .getExternalStorageDirectory().toString()
+                    + File.separator + "CloudAlbum" + File.separator);
+            if (!file.exists()) {
+                file.mkdirs();
+                Log.i(TAG, "文件夹已经创建");
+            }
+            Intent intent = new Intent(
+                    MediaStore.ACTION_IMAGE_CAPTURE);
+            File f = new File(ImageUrl);
+            intent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && requestCode == RESULT_OK) {
+            Log.i(TAG, "照相成功！");
+            todoPic();
+        }else if (requestCode == 2 && resultCode == RESULT_OK){
+            Uri selectedImage = data.getData();
+            String[] filePathColumns = { MediaStore.Images.Media.DATA };
+            Cursor c = this.getContentResolver().query(selectedImage,
+                    filePathColumns, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePathColumns[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            ImageUrl = picturePath;
+            todoPic();
+        }
+    }
+
+    private void todoPic() {
+        bitmapaddress.setText(ImageUrl);
+        final ProgressDialog dialog = CommonUtils.getProcessDialog(this, "正在检测");
+        dialog.show();
+        getData().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(RegisterActivity.this, "网络错误", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        int result = integer;
+                        switch (result) {
+                            case 1:
+                                //重复人脸
+                                Toast.makeText(RegisterActivity.this, "人脸已经注册过了", Toast.LENGTH_SHORT).show();
+                                isFaceAvaiable = false;
+                                break;
+                            case 2:
+                                //有人脸,并且不重复
+                                Toast.makeText(RegisterActivity.this, "人脸可用", Toast.LENGTH_SHORT).show();
+                                isFaceAvaiable = true;
+                                break;
+                            case 3:
+                                //没有检测到人脸
+                                Toast.makeText(RegisterActivity.this, "没有检测到人脸,请重新拍照", Toast.LENGTH_SHORT).show();
+                                isFaceAvaiable = false;
+                                break;
+                        }
+                    }
+                });
+    }
+
+    private Observable<Integer> getData() {
+        return Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                HttpRequests httpRequests = new HttpRequests(CommonUtils.FACE_API_KEY, CommonUtils.FACE_API_SECRET);
+                try {
+                    JSONObject result = httpRequests.detectionDetect(new PostParameters()
+                            .setImg(CommonUtils.ImageProcess(ImageUrl)));
+                    JSONArray ffid = result.getJSONArray("face");
+                    if (ffid.length() > 0) {
+                        //1.如果人脸存在,在从FACE)SET里检测是否已经存在该人脸
+                        JSONObject js = httpRequests
+                                .recognitionSearch(new PostParameters()
+                                        .setFacesetName(CommonUtils.FACE_SET_NAME)
+                                        .setKeyFaceId(result.getJSONArray("face").getJSONObject(0).getString("face_id")));
+                        Log.d("RegisterActivity", js.toString());
+                        JSONObject jsObject;
+                        JSONArray candidate = js.getJSONArray("candidate");
+                        if (candidate.length() < 1) {
+                            subscriber.onNext(FACE_NO);
+                            subscriber.onCompleted();
+                            return;
+                        }
+                        //遍历返回的结果
+                        for (int m = 0; m < candidate.length(); m++) {
+                            jsObject = candidate.optJSONObject(m);
+                            if (jsObject.getDouble("similarity") > 80.00) {
+                                //说明已经存在该人脸
+                                Log.i(TAG, "相似度为" + jsObject.getDouble("similarity"));
+                                subscriber.onNext(FACE_EXIST);
+                                subscriber.onCompleted();
+                                break;
+                            } else {
+                                face_id = result.getJSONArray("face").getJSONObject(0)
+                                        .getString("face_id");
+                                //没有重复人脸
+                                subscriber.onNext(FACE_NO);
+                                subscriber.onCompleted();
+                                break;
+                            }
+
+                        }
+                    } else {
+                        //没有检测到人脸
+                        subscriber.onNext(FACE_ERROR);
+                        subscriber.onCompleted();
+                    }
+                } catch (FaceppParseException e) {
+                    subscriber.onError(e);
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    subscriber.onError(e);
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * 检查2个EditText是否一致
+     *
+     * @return
+     */
+    private Boolean checkpwd() {
+
+        String s1, s2;
+        s1 = pwd.getText().toString();
+        s2 = pwdqr.getText().toString();
+        if (!(s1).equals(s2)) {
+            return true;
+        } else {
+            return false;
         }
     }
 
