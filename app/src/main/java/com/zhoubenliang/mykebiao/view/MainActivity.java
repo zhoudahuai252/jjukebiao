30cb6b3f74e36f3c04507908d79f2425a2b0eec0
zhoudada <554524787@qq.com>
2016-03-31 20:44:26 +0800

    2016.3.24.

--- app/src/main/java/com/zhoubenliang/mykebiao/view/MainActivity.java
+++ app/src/main/java/com/zhoubenliang/mykebiao/view/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        MyUser bu=new MyUser();
-        bu.signUp(this,null);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/zhoubenliang/mykebiao/view/WelcomeActivity.java
+++ app/src/main/java/com/zhoubenliang/mykebiao/view/WelcomeActivity.java
@@ -1,15 +1,78 @@
 package com.zhoubenliang.mykebiao.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.zhoubenliang.mykebiao.R;
 
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    @Bind(R.id.viewpage)
+    ViewPager viewpage;
+    @Bind(R.id.ll_root)
+    LinearLayout llRoot;
+    private ArrayList<ImageView> imageViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+        initData();
+        initViewPager();
+    }
+
+    private void initData() {
+        int[] imageId = {R.drawable.welpic1,
+                R.drawable.welpic2, R.drawable.welpic3};
+        imageViews = new ArrayList<>();
+        for (int id : imageId) {
+            ImageView imageView = new ImageView(this);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+//            imageView.setAdjustViewBounds(true);
+            imageViews.add(imageView);
+            imageView.setImageResource(id);
+        }
+    }
+
+    private void initViewPager() {
+
+        PagerAdapter adapter = new MyWelPageAdapter();
+        viewpage.setAdapter(adapter);
+    }
+
+    private class MyWelPageAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return imageViews.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = imageViews.get(position);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 }
